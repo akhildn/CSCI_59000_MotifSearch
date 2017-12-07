46fe1a7734dafa1c22397864916ffb2f08e29aef
Akhil Nayabu <akhildn@gmail.com>
2017-12-07 10:12:40 -0800

    Create MotifSearchExecutor.java

---
+++ src/MotifSearchExecutor.java
@@ -0,0 +1,92 @@
+import org.biojava.nbio.data.sequence.FastaSequence;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MotifSearchExecutor {
+
+
+
+    public static void main(String args[]) {
+
+        System.out.println("Hello World");
+
+        try {
+
+            /*
+            * Reading DNA sequences as List of Strings
+            * */
+            String fastaFileName = args[0];
+            int motifLength = Integer.parseInt(args[1]);
+            String algoType = args[2];
+
+
+            List<String> dnaList = getDNASeqStrings(fastaFileName);
+
+            if(algoType.equalsIgnoreCase("bb")) {
+                System.out.println("Branch Bound ::");
+                invokeAndMeasureBBMotifSearch(dnaList, motifLength);
+            }
+            else if(algoType.equalsIgnoreCase("greedy")) {
+                System.out.println("Modified Greedy ::");
+                invokeAndMeasureGreedyMotifSearch(dnaList, motifLength);
+            } else {
+                System.out.println("Not a Valid Algorithm name passed. Should be either bb or greedy");
+            }
+
+
+
+        } catch (IOException e) {
+            System.out.println("Error reading the Fasta File");
+            e.printStackTrace();
+        } catch (NumberFormatException ex) {
+            System.out.println("Motif Length should be integer");
+            ex.printStackTrace();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private static void invokeAndMeasureGreedyMotifSearch(List<String> dnaList, int motifLength) {
+        MotifSearchResponse response;
+        double timeTaken;
+        long startGreedyTime = System.currentTimeMillis();
+        response = GreedyMotifSearch.motifSearch(dnaList, dnaList.size(), dnaList.get(0).length(), motifLength);
+        System.out.println("Start Sequences ::");
+        for (int startIndex : response.getStartSequences()) {
+            System.out.print(startIndex + "\t");
+        }
+        long endGreedyTime = System.currentTimeMillis();
+        timeTaken = (endGreedyTime - startGreedyTime);
+
+        System.out.println("\n Time Taken for Greedy: " + timeTaken);
+    }
+
+    private static void invokeAndMeasureBBMotifSearch(List<String> dnaList, int motifLength) {
+        long startBBTime = System.currentTimeMillis();
+        MotifSearchResponse response = BBMotifSearch.branchBoundMotifSearch(dnaList, dnaList.size()
+                                                                        , dnaList.get(0).length(), motifLength);
+        System.out.println("Start Sequences ::");
+        for (int startIndex : response.getStartSequences()) {
+            System.out.print(startIndex + "\t");
+        }
+        long endBBTime = System.currentTimeMillis();
+        double timeTaken = (endBBTime - startBBTime);
+
+        System.out.println("\n Time Taken for BB: " + timeTaken);
+    }
+
+    private static List<String> getDNASeqStrings(String fastaFileName) throws IOException {
+        List<FastaSequence> fastaSeq = ProtienReader.readProtien(fastaFileName);
+        List<String> dnaList = new LinkedList<>();
+        for (FastaSequence sequence : fastaSeq) {
+            dnaList.add(sequence.getSequence());
+        }
+        return dnaList;
+    }
+
+}
